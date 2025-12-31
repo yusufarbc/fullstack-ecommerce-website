f2402a2c97fb4d0524ca4589438c98a0ebe50cb2
Yusuf Talha ARABACI <77548038+yusufarbc@users.noreply.github.com>
2025-12-31 18:52:03 +0300

    Remove monorepo project setup, dependencies, and documentation.

--- api/src/routes/categories.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { categoryController } from '../container.js';
-
-const router = express.Router();
-
-router.get('/', categoryController.getCategories);
-
-export default router;
